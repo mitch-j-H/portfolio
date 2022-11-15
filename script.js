c515a1c8abb672cc9fe26ffffcec31a065c216f0
mitch-j-H <mitchell.henwood2022@campus-eni.fr>
2022-11-15 13:41:21 +0100

    removelogs

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ window.onload = () => {
     // Checks browser language
     let browserLang = navigator.language;
     let locale = Intl.getCanonicalLocales(browserLang);
-    console.log(browserLang);
+    //redirects to appropriate page
     if(browserLang.substring(0, 2)==="fr"){
         window.location.replace("fr.index.html");
-        console.log(browserLang);
     } else {
         window.location.replace("en.index.html");
-        console.log(browserLang);
     }
 }
